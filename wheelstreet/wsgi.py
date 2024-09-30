fc569c0d5f28432ff59dcd90fb02b9e7f2bbdb0a
Jithin6055 <jithinjoseph6055@gmail.com>
2024-09-30 16:44:05 +0530

    first commit

---
+++ wheelstreet/wsgi.py
@@ -0,0 +1,19 @@
+"""
+WSGI config for wheelstreet project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+"""
+
+import sys
+import os
+
+# Add the project directory to the sys.path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wheelstreet.settings')
+application = get_wsgi_application()
